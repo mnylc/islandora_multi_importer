1f142820e121b9307808f5d3a1a03f07ab8931d5
Diego Pino Navarro <dpino@metro.org>
2018-02-21 11:25:05 -0500

    Closer

--- js/utils.js
+++ js/utils.js
@@ -8,5 +8,7 @@
  * focus an horizontal tab
  */
 Drupal.ajax.prototype.commands.focusHTab =  function(ajax, response, status) {
+// response is what we pass from PHP
     console.log(ajax);
+    console.log(response.arguments);
 }
