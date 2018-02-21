bd807bc98dcb8e13fc97ee2fda1803928ca82580
Diego Pino Navarro <dpino@metro.org>
2018-02-21 11:15:54 -0500

    JS style

---
+++ js/utils.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Tiny ajax driven UI helpegs
+ * @author Diego Pino Navarro
+ */
+
+/**
+ * focus an horizontal tab
+ */
+Drupal.ajax.prototype.commands.focusHTab =  function(ajax, response, status) {
+    console.log(ajax);
+}
