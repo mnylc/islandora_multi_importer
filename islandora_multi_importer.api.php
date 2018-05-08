99f0f5e1fac7ea89a946a857269926f4c9eaee27
Mark McFate <mcfatem@grinnell.edu>
2018-05-08 09:08:55 -0500

    Improved hook implementation

--- islandora_multi_importer.api.php
+++ islandora_multi_importer.api.php
@@ -6,36 +6,42 @@
  */
 
 /**
- * Implements hook_islandora_multi_importer_remote_file_get for https://digital.grinnell.edu.
+ * hook_islandora_multi_importer_remote_file_get example from https://digital.grinnell.edu and
+ *   https://dgadmin.grinnell.edu
  *
  * @param $url
- *   Path to the file to be fetched.  Behavior of this hook depends on the transfer scheme specified.
- *     Default behavior strips the 'basename' from $url and looks for that file somewhere in public://imi_files.
- *     @TODO... if smb:// is prepended to the $url implement a Samba file fetch here.
+ *   Path to the file to be fetched.  Default behavior strips the 'basename' from $url and looks
+ *   for that file somewhere in $target.
  * @return array
  *   On error or failure an empty array is returned.
- *   On success, a single array element LOCAL path like /var/www/drupal7/sites/default/files/imi_files/exmample.xml
+ *   On success, a single array element LOCAL path like /mnt/storage/exmample.xml
+ *
+ * Important!  This function requires that your target directory of files be mounted to
+ *   the host server as $target (see below).  The mount statement should look something
+ *   like this:
+ *
+ *  sudo mount -t cifs -o username=mcfatem //storage.grinnell.edu/LIBRARY/mcfatem/PHPP_Content /mnt/storage
+ *
  */
 function hook_islandora_multi_importer_remote_file_get($url) {
-  if ($url === '') {
-    return array();   // return an empty array
-  }
-
+  if ($url === '') { return array(); }  // return an empty array if $url is blank
   $parsed_url = parse_url($url);
 
-  // Assumes the file is somewhere in the public://imi_files path
+  // Assumes the file is somewhere in the /mnt/storage path
+  $target = "/mnt/storage";
 
   $basename = drupal_basename($parsed_url['path']);
   $pattern = "/" . preg_quote($basename) . "/";
   $options['key'] = 'filename';
-  $file = file_scan_directory("public://imi_files", $pattern, $options);
+  $file = file_scan_directory($target, $pattern, $options);
 
   $msg = "IMI hook " . __FUNCTION__ . " was invoked ";
 
   // Nothing found...report and return an empty array.
 
   if (empty($file)) {
-    $msg .= "and was unable to find a file matching '$url' for ingest.";
+    $msg .= "and was unable to find a file matching '$url' in '$target' for ingest. ";
+    $msg .= "Make sure that you have mounted your files as '$target', and that they can be read by 'www-data'!";
     drupal_set_message($msg, 'warning');
     watchdog('Islandora Multi Importer', $msg);
     return array();
@@ -45,8 +51,11 @@ function hook_islandora_multi_importer_remote_file_get($url) {
 
   else {
     $uri = $file[$basename]->uri;
-    $msg .= "and found file '$uri' for ingest.";
+    $msg .= "and found file '$uri' in '$target' for ingest.";
     drupal_set_message($msg, 'status');
     return array($uri);
   }
 }
+
+
+
